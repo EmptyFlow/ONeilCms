24983c8b4a549ac67f19da4181e55eb5d4d5118c
Roman Vladimirov <trueromanus@gmail.com>
2026-06-08 16:00:52 +0300

    Using tabs

--- src/ONielCms/Handlers/SiteBodyHandler.cs
+++ src/ONielCms/Handlers/SiteBodyHandler.cs
@@ -8,140 +8,140 @@ using ONielCms.Extensions;
 namespace ONielCms.Handlers
 {
 
-    public static class SiteBodyHandler
-    {
+	public static class SiteBodyHandler
+	{
 
-        private static Dictionary<string, RouteHandler> m_routeHandler = [];
+		private static Dictionary<string, RouteHandler> m_routeHandler = [];
 
-        public static async Task<IResult> Handler(
-            HttpContext httpContext,
-            string path,
-            IRouteResponseService routeResponseService,
-            IRouteService routeService,
-            string method,
-            IMemoryCache cache)
-        {
-            try
-            {
-                var routeHandler = m_routeHandler[method];
-                var routePair = routeHandler.GetRoute(path);
-                if (routePair != null)
-                {
-                    var route = routePair.Value.routeEntity;
-                    var response = await routeResponseService.GetResponse(path, route.Id, routeHandler.Version ?? "", httpContext.RequestAborted);
+		public static async Task<IResult> Handler(
+			HttpContext httpContext,
+			string path,
+			IRouteResponseService routeResponseService,
+			IRouteService routeService,
+			string method,
+			IMemoryCache cache)
+		{
+			try
+			{
+				var routeHandler = m_routeHandler[method];
+				var routePair = routeHandler.GetRoute(path);
+				if (routePair != null)
+				{
+					var route = routePair.Value.routeEntity;
+					var response = await routeResponseService.GetResponse(path, route.Id, routeHandler.Version ?? "", httpContext.RequestAborted);
 
-                    var processors = route.GetProcessors();
-                    if (processors.Any())
-                    {
-                        var state = CreateState(httpContext, cache);
+					var processors = route.GetProcessors();
+					if (processors.Any())
+					{
+						var state = CreateState(httpContext, cache);
 
-                        foreach (var processor in processors)
-                        {
-                            if (m_textProcessors.TryGetValue(processor.Name, out var processorAction))
-                            {
-                                processorAction?.Invoke(ref state);
-                                if (!state.Handled && state.Result is not null) return state.Result;
-                                if (!state.Handled) return Results.StatusCode(500);
-                            }
-                        }
+						foreach (var processor in processors)
+						{
+							if (m_textProcessors.TryGetValue(processor.Name, out var processorAction))
+							{
+								processorAction?.Invoke(ref state);
+								if (!state.Handled && state.Result is not null) return state.Result;
+								if (!state.Handled) return Results.StatusCode(500);
+							}
+						}
 
-                        if (state.Result != null) return state.Result;
-                    }
+						if (state.Result != null) return state.Result;
+					}
 
-                    return Results.File(response.Item1, route.ContentType, fileDownloadName: !string.IsNullOrEmpty(route.DownloadFileName) ? route.DownloadFileName : null);
-                }
-                if (routePair == null) return Results.NotFound();
+					return Results.File(response.Item1, route.ContentType, fileDownloadName: !string.IsNullOrEmpty(route.DownloadFileName) ? route.DownloadFileName : null);
+				}
+				if (routePair == null) return Results.NotFound();
 
-                return Results.Ok();
-            }
-            catch (StatusCodeException statusCodeException)
-            {
-                return Results.StatusCode(statusCodeException.StatusCode);
+				return Results.Ok();
+			}
+			catch (StatusCodeException statusCodeException)
+			{
+				return Results.StatusCode(statusCodeException.StatusCode);
 #if DEBUG
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
 #else
             } catch {
 #endif
-                return Results.StatusCode(500);
-            }
-        }
+				return Results.StatusCode(500);
+			}
+		}
 
-        private static void FillHandler(string version, IEnumerable<SiteRoute> routes, Dictionary<string, RouteHandler> handlerDictionary)
-        {
-            if (!routes.Any()) return;
+		private static void FillHandler(string version, IEnumerable<SiteRoute> routes, Dictionary<string, RouteHandler> handlerDictionary)
+		{
+			if (!routes.Any()) return;
 
-            var handler = new RouteHandler();
-            handler.FillRoutesCache(version, routes);
-            var method = routes.First().Method;
+			var handler = new RouteHandler();
+			handler.FillRoutesCache(version, routes);
+			var method = routes.First().Method;
 
-            handlerDictionary.Add(method, handler);
-        }
+			handlerDictionary.Add(method, handler);
+		}
 
-        public static async Task LoadAllRoutesInCurrentVersion(IConfigurationService configurationService)
-        {
-            Dictionary<string, RouteHandler> handlers = new();
-            var storageContext = new StorageContext(new ConsoleStorageLogger(), configurationService);
-            var routeService = new RouteService(storageContext);
+		public static async Task LoadAllRoutesInCurrentVersion(IConfigurationService configurationService)
+		{
+			Dictionary<string, RouteHandler> handlers = new();
+			var storageContext = new StorageContext(new ConsoleStorageLogger(), configurationService);
+			var routeService = new RouteService(storageContext);
 
-            var (routes, version) = await routeService.GetAllRoutesInCurrentVersion();
+			var (routes, version) = await routeService.GetAllRoutesInCurrentVersion();
 
-            foreach (var group in routes.GroupBy(a => a.Method))
-            {
-                FillHandler(version, group, handlers);
-            }
+			foreach (var group in routes.GroupBy(a => a.Method))
+			{
+				FillHandler(version, group, handlers);
+			}
 
-            m_routeHandler = handlers;
-        }
+			m_routeHandler = handlers;
+		}
 
-        private static readonly Dictionary<string, RouteProcessorDelegate> m_textProcessors = [];
+		private static readonly Dictionary<string, RouteProcessorDelegate> m_textProcessors = [];
 
-        public delegate ValueTask RouteProcessorDelegate(ref ProcessorState processorState);
+		public delegate ValueTask RouteProcessorDelegate(ref ProcessorState processorState);
 
-        private static ProcessorState CreateState(HttpContext context, IMemoryCache memoryCache)
-        {
-            var flags = new Dictionary<string, bool>();
-            return new ProcessorState
-            {
-                HttpContext = context,
-                MemoryCache = memoryCache,
-                Result = null,
-                Handled = true,
-                Flags = flags
-            };
-        }
+		private static ProcessorState CreateState(HttpContext context, IMemoryCache memoryCache)
+		{
+			var flags = new Dictionary<string, bool>();
+			return new ProcessorState
+			{
+				HttpContext = context,
+				MemoryCache = memoryCache,
+				Result = null,
+				Handled = true,
+				Flags = flags
+			};
+		}
 
-        /// <summary>
-        /// Add processor for text content.
-        /// </summary>
-        /// <param name="processor">Processor name.</param>
-        /// <param name="callback">Callback.</param>
-        public static void AddRouteProcessor(string processor, RouteProcessorDelegate callback)
-        {
-            if (m_textProcessors.ContainsKey(processor)) throw new Exception($"Text processor with name {processor} already added!");
+		/// <summary>
+		/// Add processor for text content.
+		/// </summary>
+		/// <param name="processor">Processor name.</param>
+		/// <param name="callback">Callback.</param>
+		public static void AddRouteProcessor(string processor, RouteProcessorDelegate callback)
+		{
+			if (m_textProcessors.ContainsKey(processor)) throw new Exception($"Text processor with name {processor} already added!");
 
-            m_textProcessors.Add(processor, callback);
-        }
+			m_textProcessors.Add(processor, callback);
+		}
 
-        public struct ProcessorState
-        {
+		public struct ProcessorState
+		{
 
-            public HttpContext HttpContext;
+			public HttpContext HttpContext;
 
-            public IMemoryCache MemoryCache;
+			public IMemoryCache MemoryCache;
 
-            public IResult? Result;
+			public IResult? Result;
 
-            public bool Handled;
+			public bool Handled;
 
-            public Dictionary<string, bool> Flags;
+			public Dictionary<string, bool> Flags;
 
-            public readonly bool IsAuthenticated() => Flags["Authenticated"];
+			public readonly bool IsAuthenticated() => Flags["Authenticated"];
 
-        }
+		}
 
-    }
+	}
 
 }
--- src/ONielCms/Services/CommandLineHandler.cs
+++ src/ONielCms/Services/CommandLineHandler.cs
@@ -2,73 +2,86 @@
 using ONielCms.Services.DatabaseLogic;
 using ONielCommon.Storage;
 
-namespace ONielCms.Services {
+namespace ONielCms.Services
+{
 
-    public class ImportCommand {
+	public class ImportCommand
+	{
 
-        public string Path { get; set; } = "";
+		public string Path { get; set; } = "";
 
-        public string Version { get; set; } = "";
+		public string Version { get; set; } = "";
 
-    }
+	}
 
-    public static class CommandLineHandler {
+	public static class CommandLineHandler
+	{
 
-        public static async Task<bool> HandleCommandLine ( IStorageContext storageContext ) {
-            var result = await CommandLine.Console ()
-                .Application ( "OnielCms", "1.0.1", "Web content management system.", "Copyright (c) 2025 Roman Vladimirov", "onielcms" )
-                .AddAsyncCommand (
-                    "import-file",
-                    async ( ImportCommand parameters ) => {
-                        var service = new ImportVersionService ( storageContext );
-                        try {
-                            await service.ImportFromFile ( parameters.Path );
-                        } catch ( Exception ex ) {
-                            Console.WriteLine ( ex.Message );
-                        }
-                    },
-                    "Import new version from file",
-                    new List<FlowCommandParameter> {
-                        FlowCommandParameter.CreateRequired(name: "p", alias: "path", help: "Path to folder where containing new version"),
-                    }
-                )
-                .AddAsyncCommand (
-                    "import-folder",
-                    async ( ImportCommand parameters ) => {
-                        var service = new ImportVersionService ( storageContext );
-                        try {
-                            await service.ImportFromFolder ( parameters.Path, parameters.Version );
-                        } catch ( Exception ex ) {
-                            Console.WriteLine ( ex.Message );
-                        }
-                    },
-                    "Import new version from specified folder",
-                    new List<FlowCommandParameter> {
-                        FlowCommandParameter.CreateRequired(name: "p", alias: "path", help: "Path to folder where need import files"),
-                        FlowCommandParameter.CreateRequired(name: "v", alias: "version", help: "Version related to "),
-                    }
-                )
-                .AddAsyncCommand(
-                    "remove-all",
-                    async (ImportCommand parameters) => {
-                        var service = new ImportVersionService(storageContext);
-                        try
-                        {
-                            await service.RemoveAllVersions();
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                    },
-                    "Remove all versions",
-                    []
-                )
-                .RunCommandAsync ();
+		public static async Task<bool> HandleCommandLine(IStorageContext storageContext)
+		{
+			var result = await CommandLine.Console()
+				.Application("OnielCms", "1.0.1", "Web content management system.", "Copyright (c) 2025 Roman Vladimirov", "onielcms")
+				.AddAsyncCommand(
+					"import-file",
+					async (ImportCommand parameters) =>
+					{
+						var service = new ImportVersionService(storageContext);
+						try
+						{
+							await service.ImportFromFile(parameters.Path);
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+						}
+					},
+					"Import new version from file",
+					new List<FlowCommandParameter> {
+						FlowCommandParameter.CreateRequired(name: "p", alias: "path", help: "Path to folder where containing new version"),
+					}
+				)
+				.AddAsyncCommand(
+					"import-folder",
+					async (ImportCommand parameters) =>
+					{
+						var service = new ImportVersionService(storageContext);
+						try
+						{
+							await service.ImportFromFolder(parameters.Path, parameters.Version);
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+						}
+					},
+					"Import new version from specified folder",
+					new List<FlowCommandParameter> {
+						FlowCommandParameter.CreateRequired(name: "p", alias: "path", help: "Path to folder where need import files"),
+						FlowCommandParameter.CreateRequired(name: "v", alias: "version", help: "Version related to "),
+					}
+				)
+				.AddAsyncCommand(
+					"remove-all",
+					async (ImportCommand parameters) =>
+					{
+						var service = new ImportVersionService(storageContext);
+						try
+						{
+							await service.RemoveAllVersions();
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+						}
+					},
+					"Remove all versions",
+					[]
+				)
+				.RunCommandAsync();
 
-            return !result.EmptyInput && ( result.CommandHandled || result.Handled );
-        }
+			return !result.EmptyInput && (result.CommandHandled || result.Handled);
+		}
 
-    }
+	}
 
 }
