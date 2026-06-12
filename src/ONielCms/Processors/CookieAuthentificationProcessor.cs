c0359cdf1084ce40c0779f70ce1f8592da4b28f1
Roman Vladimirov <trueromanus@gmail.com>
2026-06-12 12:16:27 +0300

    Fixed tabs

--- src/ONielCms/Processors/CookieAuthentificationProcessor.cs
+++ src/ONielCms/Processors/CookieAuthentificationProcessor.cs
@@ -5,49 +5,49 @@ using static OnielCms.Core.HttpRouteHandler;
 namespace ONielCms.Processors
 {
 
-    /// <summary>
-    /// Cookie authentification checker.
-    /// </summary>
-    public static class CookieAuthentificationProcessor
-    {
+	/// <summary>
+	/// Cookie authentification checker.
+	/// </summary>
+	public static class CookieAuthentificationProcessor
+	{
 
-        private const string DefaultCookieKey = "capst";
+		private const string DefaultCookieKey = "capst";
 
-        public static ValueTask CheckAndExitAuthentification(ref ProcessorState state, ProcessorElement processorElement)
-        {
-            var cookieKey = GetCookieKey(processorElement);
-            var token = state.HttpContext.Request.Cookies.Where(a => a.Key == cookieKey).Select(a => a.Value).FirstOrDefault();
-            if (string.IsNullOrEmpty(token) || !state.MemoryCache.TryGetValue(token, out _))
-            {
-                state.Handled = false;
-                state.Result = Results.StatusCode(401);
-                return ProcessorsShared.EmptyValueTask;
-            }
+		public static ValueTask CheckAndExitAuthentification(ref ProcessorState state, ProcessorElement processorElement)
+		{
+			var cookieKey = GetCookieKey(processorElement);
+			var token = state.HttpContext.Request.Cookies.Where(a => a.Key == cookieKey).Select(a => a.Value).FirstOrDefault();
+			if (string.IsNullOrEmpty(token) || !state.MemoryCache.TryGetValue(token, out _))
+			{
+				state.Handled = false;
+				state.Result = Results.StatusCode(401);
+				return ProcessorsShared.EmptyValueTask;
+			}
 
-            state.Handled = true;
-            return ProcessorsShared.EmptyValueTask;
-        }
+			state.Handled = true;
+			return ProcessorsShared.EmptyValueTask;
+		}
 
-        public static ValueTask CheckAuthentification(ref ProcessorState state, ProcessorElement processorElement)
-        {
-            var cookieKey = GetCookieKey(processorElement);
-            var token = state.HttpContext.Request.Cookies.Where(a => a.Key == cookieKey).Select(a => a.Value).FirstOrDefault();
-            state.Handled = true;
-            if (string.IsNullOrEmpty(token) || !state.MemoryCache.TryGetValue(token, out _))
-            {
-                state.Flags["Authenticated"] = true;
-                return ProcessorsShared.EmptyValueTask;
-            }
+		public static ValueTask CheckAuthentification(ref ProcessorState state, ProcessorElement processorElement)
+		{
+			var cookieKey = GetCookieKey(processorElement);
+			var token = state.HttpContext.Request.Cookies.Where(a => a.Key == cookieKey).Select(a => a.Value).FirstOrDefault();
+			state.Handled = true;
+			if (string.IsNullOrEmpty(token) || !state.MemoryCache.TryGetValue(token, out _))
+			{
+				state.Flags["Authenticated"] = true;
+				return ProcessorsShared.EmptyValueTask;
+			}
 
-            return ProcessorsShared.EmptyValueTask;
-        }
+			return ProcessorsShared.EmptyValueTask;
+		}
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static string GetCookieKey(ProcessorElement processorElement)
-        {
-            return processorElement.Parameters.FirstOrDefault(a => a.Name == "CookieKey")?.Value ?? DefaultCookieKey;
-        }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static string GetCookieKey(ProcessorElement processorElement)
+		{
+			return processorElement.Parameters.FirstOrDefault(a => a.Name == "CookieKey")?.Value ?? DefaultCookieKey;
+		}
 
-    }
+	}
 
 }
--- src/ONielCms/Processors/RedirectsProcessors.cs
+++ src/ONielCms/Processors/RedirectsProcessors.cs
@@ -4,34 +4,34 @@ using static OnielCms.Core.HttpRouteHandler;
 namespace ONielCms.Processors
 {
 
-    public static class RedirectsProcessors
-    {
+	public static class RedirectsProcessors
+	{
 
-        public static ValueTask TemporaryRedirect(ref ProcessorState state, ProcessorElement processorElement)
-        {
-            string redirectUrl = GetRedirectUrl(processorElement);
+		public static ValueTask TemporaryRedirect(ref ProcessorState state, ProcessorElement processorElement)
+		{
+			string redirectUrl = GetRedirectUrl(processorElement);
 
-            state.Result = Results.Redirect(redirectUrl, false, true);
-            state.Handled = true;
-            return ProcessorsShared.EmptyValueTask;
-        }
+			state.Result = Results.Redirect(redirectUrl, false, true);
+			state.Handled = true;
+			return ProcessorsShared.EmptyValueTask;
+		}
 
-        public static ValueTask TemporaryRedirectWithoutBody(ref ProcessorState state, ProcessorElement processorElement)
-        {
-            string redirectUrl = GetRedirectUrl(processorElement);
+		public static ValueTask TemporaryRedirectWithoutBody(ref ProcessorState state, ProcessorElement processorElement)
+		{
+			string redirectUrl = GetRedirectUrl(processorElement);
 
-            state.HttpContext.Response.Redirect(redirectUrl, false, false);
-            state.Handled = true;
-            return ProcessorsShared.EmptyValueTask;
-        }
+			state.HttpContext.Response.Redirect(redirectUrl, false, false);
+			state.Handled = true;
+			return ProcessorsShared.EmptyValueTask;
+		}
 
-        private static string GetRedirectUrl(ProcessorElement processorElement)
-        {
-            var redirectUrl = processorElement.Parameters.FirstOrDefault(a => a.Name == "Url")?.Value ?? "";
-            if (string.IsNullOrEmpty(redirectUrl)) throw new Exception("TemporaryRedirect: Url parameter is required to perform this action!");
-            return redirectUrl;
-        }
+		private static string GetRedirectUrl(ProcessorElement processorElement)
+		{
+			var redirectUrl = processorElement.Parameters.FirstOrDefault(a => a.Name == "Url")?.Value ?? "";
+			if (string.IsNullOrEmpty(redirectUrl)) throw new Exception("TemporaryRedirect: Url parameter is required to perform this action!");
+			return redirectUrl;
+		}
 
-    }
+	}
 
 }
